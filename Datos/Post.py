60d3cf8c5ab39c9c28520051846b71aeef78326c
Jonatan Gonzalez <3005959480101@ingenieria.usac.edu.gt>
2020-10-23 16:22:55 -0600

    Coment and reaccions

---
+++ Datos/Post.py
@@ -0,0 +1,6 @@
+class Post:
+  def __init__(self, tituloReceta, reaccion,usuario,coment):
+    self.tituloReceta = tituloReceta
+    self.reaccion = reaccion
+    self.usuario = usuario
+    self.coment = coment
