34c8d5dd4dc1df0ef0a8280f13a6c8b5bb7ec06f
Jonatan Gonzalez <3005959480101@ingenieria.usac.edu.gt>
2020-10-25 01:04:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def RegistrarReceta():
 
         recetas.append(Receta(autor,titulo,resumen,ingredientes,procedimiento,tiempo,imagen)) 
         confirm = 'La receta se agrego correctamente'
-        render_template('RegistrarReceta.html' , confirm = confirm)
+        
         return Inicio()      
     return render_template('RegistrarReceta.html', confirm = confirm )        
 
